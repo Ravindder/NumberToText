49b3bda844b6269e0aca696e66aa4642e87aa0b1
Ravinder <30561638+Ravindder@users.noreply.github.com>
2019-08-23 00:01:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberToText.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isNegative = "";
+            try
+            {
+                Console.WriteLine("Enter a Number");
+                string number = Console.ReadLine();
+                number = Convert.ToDouble(number).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    Console.WriteLine("The number in text is \nZero");
+                }
+                else
+                {
+                    Console.WriteLine("The number in text is \n{0}", isNegative + NumberToTextHelper.ConvertToWords(number));
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
